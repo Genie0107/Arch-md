1fcef150b814e987256dd1e17136f0e2e72e7f22
Genie0107 <156694814+Genie0107@users.noreply.github.com>
2024-12-15 11:16:16 +0100

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const chalk = require("chalk")
 const fs = require("fs")
 
 //======= Change settings =======\\
-global.owner = ["2347036214381"] // owner number 
+global.owner = ["2348081585553"] // owner number 
 global.botname = ["Arch Md"] // don't change 
 global.simbol = "♘" // don't change 
 global.typeMenu = 'v1' //don't change 
